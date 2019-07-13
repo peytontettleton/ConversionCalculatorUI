6045c3a07e80fe52044b7d8d9d52c52f65f81c11
Peyton Tettleton <peytontettleton@me.com>
2019-07-12 21:50:01 -0500

    Creation

---
+++ ConversionCalculatorUI/ConversionCalculatorUIViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConversionCalculatorUIViewController.swift
+//  ConversionCalculatorUI
+//
+//  Created by Peyton Tettleton on 7/11/19.
+//  Copyright © 2019 Peyton Tettleton. All rights reserved.
+//
+
+import UIKit
+
+class ConversionCalculatorUIViewController: UIViewController {
+
+    @IBOutlet weak var outputDisplay: UITextField!
+    @IBOutlet weak var inputDisplay: UITextField!
+    @IBAction func converterButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ConversionCalculatorUI/Structs.swift
@@ -0,0 +1,14 @@
+//
+//  Structs.swift
+//  ConversionCalculatorUI
+//
+//  Created by Peyton Tettleton on 7/12/19.
+//  Copyright © 2019 Peyton Tettleton. All rights reserved.
+//
+
+import Foundation
+
+
+struct fahrenheit to celcius {
+    //I have absolutely no idea what to do in this struct im not gonna lie. I feel like I couldve completed this project if I knew what to put here though.
+}
